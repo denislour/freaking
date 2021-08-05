e00e936af7453ad8c5f7832e7a511ab68dfc6d13
Lektor <denislour@gmail.com>
2021-08-05 18:55:27 +0700

    Scanning the source folder and parse all this files to dest folder.

--- freaking.py
+++ freaking.py
@@ -1,9 +1,14 @@
 import typer
 from freaking.core import Freaking
+from freaking.parser import ResourceParser
 
 
 def main(source: str = "content", dest: str = "dest"):
-    config: dict = {"source": source, "dest": dest}
+    config: dict = {
+        "source": source,
+        "dest": dest,
+        "parsers": list(ResourceParser()),
+    }
     Freaking(**config).build()
 
 
--- freaking/core.py
+++ freaking/core.py
@@ -1,19 +1,36 @@
 from pathlib import Path
+from typing import List
+from freaking.parser import ResourceParser
 
 
 class Freaking:
-    def __init__(self, source: str, dest: str):
+    def __init__(self, source: str, dest: str, parsers: List[ResourceParser]):
         """Constructure for core instance"""
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers
 
     def create_dir(self, path: Path):
         """Create directory from path"""
         dir = self.dest / path.relative_to(self.source)
         dir.mkdir(parents=True, exist_ok=True)
 
+    def load_parser(self, extension: str):
+        for parser in self.parsers:
+            if parser.valid_extensions(extension):
+                return parser
+
+    def run_parser(self, path: Path):
+        parser = self.load_parser(path.suffix)
+        if parser:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
+
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
